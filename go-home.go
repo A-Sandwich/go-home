e97343096aa140ef9d2c80a8b58f2b08e9baa706
Kyle Burkholder <kbburkholder@gmail.com>
2018-11-17 12:41:53 -0500

    Cleaned up code.

--- go-home.go
+++ go-home.go
@@ -19,17 +19,18 @@ type countyStruct struct {
     Time string `xml:"posted_date"`
 }
 
+type emailStruct struct {
+    Sender string
+    Password string
+    Recipient string
+    County string
+    MinuteDelta int
+    Message string
+    Subject string
+}
 
 func main() {
-    countyptr := flag.String("County", "Hamilton", "County you want to know the weather for.")
-    minuteptr := flag.Int("Minutes", 15, "How often you want to check for weather updates.")
-    senderemailptr := flag.String("e-mail", "", "e-mail to send notification emails. (Enable less secure apps.)")
-    passwordptr := flag.String("e-mail password", "", "Password for your sending e-mail")
-    recipientemailptr := flag.String("e-mail", "", "e-mail to send notification emails. (Enable less secure apps.)")
-    flag.Parse()
-
-    fmt.Println(*countyptr)
-    fmt.Println(*minuteptr)
+    emailData := parseFlags()
 
     for {
         resp, err := http.Get("https://www.in.gov/ai/dhs/dhs_travel_advisory.txt")
@@ -50,43 +51,53 @@ func main() {
             fmt.Println("Unable to unmrashal XML")
         } else {
             for _, element := range counties.Counties {
-                if element.County == *countyptr {
+                if element.County == emailData.County {
                     fmt.Println(element.County)
                     fmt.Println(element.Status)
                     fmt.Println(element.Time)
-                    if *senderemailptr != "" {
-                        message := "The county " + element.County +
+                    if emailData.Sender != "" {
+                        emailData.Message = "The county " + element.County +
                                    " has the weather status of " + element.Status +
                                    " at " + element.Time
-                        subject := element.County + ": " + element.Status
-                        send(subject, message, *senderemailptr, *passwordptr, *recipientemailptr)
+                        emailData.Subject = element.County + ": " + element.Status
+                        send(emailData)
                     }
                 }
-
             }
         }
 
-        time.Sleep(time.Duration(*minuteptr) * time.Minute)
+        time.Sleep(time.Duration(emailData.MinuteDelta) * time.Minute)
     }
 }
 
-func send(subject string, body string, sender string, password string, recipient string) {
-    from := sender
-    pass := password
-    to := recipient
-
-    msg := "From: " + from + "\n" +
-        "To: " + to + "\n" +
-        subject +
-        body
+func send(email emailStruct) {
+    msg := "From: " + email.Sender + "\n" +
+        "To: " + email.Recipient + "\n" +
+        email.Subject +
+        email.Message
 
     err := smtp.SendMail("smtp.gmail.com:587",
-        smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-        from, []string{to}, []byte(msg))
+        smtp.PlainAuth("", email.Sender, email.Password, "smtp.gmail.com"),
+        email.Sender, []string{email.Recipient}, []byte(msg))
 
     if err != nil {
-//        log.Printf("smtp error: %s", err)
         return
     }
- //   log.Print("sent, visit http://foobarbazz.mailinator.com")
+}
+
+func parseFlags() emailStruct {
+    countyptr := flag.String("County", "Hamilton", "County you want to know the weather for.")
+    minuteptr := flag.Int("Minutes", 15, "How often you want to check for weather updates.")
+    senderemailptr := flag.String("e-mail", "", "e-mail to send notification emails. (Enable less secure apps.)")
+    passwordptr := flag.String("e-mail password", "", "Password for your sending e-mail")
+    recipientemailptr := flag.String("e-mail", "", "e-mail to send notification emails. (Enable less secure apps.)")
+    flag.Parse()
+
+    return emailStruct{
+        Sender: *senderemailptr,
+        Password: *passwordptr,
+        Recipient: *recipientemailptr,
+        County: *countyptr,
+        MinuteDelta: *minuteptr,
+    }
 }
